399f867096ed75e48291068f5eee31144148c219
xiuyuma <ma79@wisc.edu>
2022-03-30 17:10:42 -0400

    unit test

---
+++ tests/testthat/test_cal_D.R
@@ -0,0 +1,9 @@
+context("cal_D")
+test_that("PDD gives correct result",{
+    data(sim_dat)
+    data_counts <- assays(sim_dat)$counts
+    conditions <- colData(sim_dat)$conditions
+    rownames(data_counts) <- seq_len(1000)
+    D_c <- cal_D(data_counts,1)
+    expect_equal(D_c[100,101],0.01811737,tolerance=1e-6)
+})
