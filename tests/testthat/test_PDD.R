1a6590ccf7c061e0f3a81bac295ca096bb3b3736
xiuyuma <ma79@wisc.edu>
2022-04-01 12:52:38 -0400

    fix to no random

--- tests/testthat/test_PDD.R
+++ tests/testthat/test_PDD.R
@@ -5,6 +5,6 @@ test_that("PDD gives correct result",{
     conditions <- colData(sim_dat)$conditions
     rownames(data_counts) <- seq_len(1000)
     D_c <- cal_D(data_counts,1)
-    pdd <- PDD(data = data_counts,cd = conditions, ncores = 1, D = D_c)
-    expect_equal(as.numeric(pdd[101]),0.005305089,tolerance=1e-6)
+    pdd <- PDD(data = data_counts,cd = conditions, ncores = 1, D = D_c, random = FALSE)
+    expect_equal(as.numeric(pdd[101]),0.008143924,tolerance=1e-6)
 })
