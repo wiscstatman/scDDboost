fabcb4a9d58c4544845f1f324a0c992852f46f9d
xiuyuma <ma79@wisc.edu>
2022-04-26 12:37:45 -0400

    rename

---
+++ R/mcp.R
@@ -0,0 +1,25 @@
+
+## ------------------------------------------------------------------------------------
+##          Internal function -- modal-cluster for poisson gamma model
+## ------------------------------------------------------------------------------------
+
+## x an integer vector
+## y mass parameter
+## z shape and scale parameters for gamma distribution
+## return cluster of x
+
+mcp <- function(x,y = 1,z = c(1,1)) {
+    if(!is.vector(x)){
+        stop("cluster object must be vector")
+    }
+    if(sum(x %% 1) != 0){
+        stop("x must be vector of integers")
+    }
+    if(length(z) != 2){
+        stop("incorrect number of hyper parameters")
+    }
+    fit<-.Call("MCP",X = x,MASS = y, PARAM=z)
+    return(fit)
+}
+
+
