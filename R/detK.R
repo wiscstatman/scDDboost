f5725b174b3342d74723da3d8db1af48e18443c9
xiuyuma <ma79@wisc.edu>
2022-04-26 11:14:50 -0400

    rm unused codes

--- R/detK.R
+++ R/detK.R
@@ -21,14 +21,10 @@ detK <- function(D, epi = 1)
          clusRes <- pam(D,i,diss = TRUE)
          intra[i - 1] <- as.numeric(clusRes$objective[1])
          x <- clusRes$id.med
-         #inter[i - 1] = sum(D[x,x]) / (i * (i - 1))
          inter[i - 1] <- mean(D[x,x])
      }
     
     s <- intra / inter
-    
-    #mins = min(s)
-    #ss = s/mins - 1
     
     ss <- s
     
