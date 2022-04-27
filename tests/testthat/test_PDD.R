2dd9e000ab37ea6b327a654eaf14e8557e5b1b35
xiuyuma <ma79@wisc.edu>
2022-04-27 13:59:56 -0400

    rename

--- tests/testthat/test_PDD.R
+++ tests/testthat/test_PDD.R
@@ -1,10 +1,11 @@
-context("PDD")
-test_that("PDD gives correct result",{
+context("pdd")
+test_that("pdd gives correct result",{
     data(sim_dat)
     data_counts <- assays(sim_dat)$counts
     conditions <- colData(sim_dat)$conditions
     rownames(data_counts) <- seq_len(1000)
-    D_c <- cal_D(data_counts,1)
-    pdd <- PDD(data = data_counts,cd = conditions, ncores = 1, D = D_c, random = FALSE)
+    bp <- BiocParallel::MulticoreParam(2)
+    D_c <- calD(data_counts,bp)
+    pdd <- pdd(data = data_counts,cd = conditions, bp, D = D_c, random = FALSE)
     expect_equal(as.numeric(pdd[101]),0.008143924,tolerance=1e-6)
 })
--- tests/testthat/test_cal_D.R
+++ tests/testthat/test_cal_D.R
@@ -1,9 +1,10 @@
-context("cal_D")
-test_that("PDD gives correct result",{
+context("calD")
+test_that("pdd gives correct result",{
     data(sim_dat)
     data_counts <- assays(sim_dat)$counts
     conditions <- colData(sim_dat)$conditions
     rownames(data_counts) <- seq_len(1000)
-    D_c <- cal_D(data_counts,1)
+    bp <- BiocParallel::MulticoreParam(2)
+    D_c <- calD(data_counts,bp)
     expect_equal(as.numeric(D_c[100,101]),0.01811737,tolerance=1e-6)
 })
